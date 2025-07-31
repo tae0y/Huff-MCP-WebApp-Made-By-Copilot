8497c57acbf139c6cc8a0177c2a5f649a23ed896
박태영(tae0y) <tae0yp@gmail.com>
2025-07-31 22:46:43 +0900

    😎 빌드오류: @@media, 사라진 클래스 참조 해결

--- src/WebApp.Web/Program.cs
+++ src/WebApp.Web/Program.cs
@@ -19,13 +19,6 @@ builder.Services.AddScoped<IConfigurationService, ConfigurationService>();
 
 builder.Services.AddOutputCache();
 
-builder.Services.AddHttpClient<WeatherApiClient>(client =>
-    {
-        // This URL uses "https+http://" to indicate HTTPS is preferred over HTTP.
-        // Learn more about service discovery scheme resolution at https://aka.ms/dotnet/sdschemes.
-        client.BaseAddress = new("https+http://apiservice");
-    });
-
 var app = builder.Build();
 
 if (!app.Environment.IsDevelopment())
